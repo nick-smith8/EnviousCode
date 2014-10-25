2f5e4a26d8e11e212eecca9dbe5b13fa749ba97f
Falcon Taylor-Carter <falconertc@colorado.edu>
2014-10-25 17:00:31 -0600

    Added mongodb, added /userlist as test

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/eonline');
 
 var app = express();
 
@@ -21,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,17 @@ router.get('/', function(req, res) {
 });
 
 router.get('/test', function(req, res) {
-  res.render('test', { title: 'Express' });
+  res.render('test', { title: 'Hello World' });
+});
+
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
 });
 
 module.exports = router;
