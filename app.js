54f9bb1dd859bdcb89072be877e3be6297bb1854
Falcon Taylor-Carter <falconertc@colorado.edu>
2014-12-02 22:09:23 -0500

    Pointless change to test git unpacker error

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 
-//Edit it
 //var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
