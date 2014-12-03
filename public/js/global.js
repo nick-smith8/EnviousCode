0e203201ca012a24771ce08efb22874216bc6fe1
Falcon Taylor-Carter <falconertc@colorado.edu>
2014-12-03 14:10:14 -0500

    Added ability to create accounts

--- public/js/global.js
+++ public/js/global.js
@@ -21,13 +21,31 @@ $('.login-submit').click(function(event) {
   alert("BOO");
 });
 
+$('.create-account').click(function(event) {
+  window.location.href += 'create'
+});
+
+$('.return-to-signin').click(function(event) {
+  window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')+1)
+});
+
 //Confirm new account
 $('.create-submit').click(function(event) {
 	event.preventDefault();
 	//Post go here
-
-	$.post( 'users/create_account', {name: "Falcon"}, function(data) {
-		alert(data);
+	var user = $('.createform #newuser').val();
+	var pass = $('.createform #newpass1').val();
+	var confirmpass = $('.createform #newpass2').val();
+    // Reset form
+	$('.createform #newpass1').val('');
+	$('.createform #newpass2').val('');
+	$.post( 'users/create_account', {user: user, pass: pass, confirmpass: confirmpass}, function(data) {
+		if (data === '0') {
+			alert('Account created successfully');
+			$('.return-to-signin').click();
+		}
+		else
+			alert(data);
 	});
 })
 
--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,6 @@ router.use(function(req, res, next) {
 });
 
 router.get('/get_highscores', function(req, res) {
-  res.send("Text");
-  return;
-
   var db = req.db;
   var collection = db.get('highscores');
   collection.find({}, {sort : {score : -1}}, function (e, items) {
@@ -21,8 +18,33 @@ router.get('/get_highscores', function(req, res) {
   });
 });
 
+// Validate new account values and insert if valid
 router.post('/create_account', function(req, res) {
-  res.send(req.body.name);
+  var user = req.body.user;
+  var pass1 = req.body.pass;
+  var pass2 = req.body.confirmpass;
+  console.log(user);
+  if (!pass1 || !pass2 || pass1 !== pass2){
+  	console.log(pass1 + " " + pass2);
+  	res.send("Invalid passwords received");
+  	return;
+  }
+  var db = req.db;
+  var collection = db.get('highscores');
+  collection.count({ username: user }, function(error, count) {
+  	if (count === 0) {
+  		console.log("Inserting username: "+user);
+  		collection.insert({username: user, pass: pass1, score: 0}, function(err, result) {
+  			if(err) 
+  				res.send(err)
+  			else
+  				res.send('0');
+  		});
+  	}
+  	else {
+  		res.send("Username already exists");
+   }
+  });
 });
 
 
