fda81a382dc4d893c8045d6975a64bd41ddfbbe2
Falcon Taylor-Carter <falconertc@colorado.edu>
2014-12-08 13:56:07 -0500

    Removed old, unused files

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,6 @@ router.get('/create', function(req, res) {
   res.render('create_account', {});
 });
 
-router.get('/table', function(req, res) {
-  res.render('table', { title: 'Envious Online' });
-});
-
 router.get('/game', function(req, res) {
   var username = req.cookies.username;
   res.render('game', { username : username });
@@ -22,50 +18,6 @@ router.get('/game', function(req, res) {
 
 router.get('/lobby', function(req, res) {
   res.render('lobby', {});
-});
-
-
-
-router.get('/test', function(req, res) {
-  res.render('test', { title: 'Hello World' });
-});
-
-router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.find({},{},function(e,docs){
-    res.render('userlist', {
-      "userlist" : docs
-    });
-  });
-});
-
-router.get('/adduser', function(req, res) {
-  res.render('adduser', { title: 'Add New User' });
-});
-
-router.get('/EnviousOnline', function(req, res) {
-  res.render('game.jade');
-});
-
-router.post('/addusersubmit', function(req, res) {
-    var db = req.db;
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-    var collection = db.get('usercollection');
-
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            res.location("userlist");
-            res.redirect("userlist");
-        }
-    });
 });
 
 module.exports = router;
